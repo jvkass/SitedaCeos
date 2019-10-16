05a4deb918fa7fed74230b338560e784aeec3076
Sindyme <cinthiaadm15@gmail.com>
2019-10-16 20:08:02 -0300

    1° commit test

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import './App.css';
 import Routes from './Routes.js';
 import Navbar from './components/Navbar/Navbar.js';
 import Footer from './components/Footer/Footer.js';
-
-
 class App extends Component {
   render() {
     return (
---
+++ src/components/Contato/Contato.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import './Contato.css';
+import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+export default class Contato extends React.Component{
+
+    render(){
+        return(
+            <section className="formulario">
+            <div>
+            <h1>Entre em contato!</h1>
+            <form  className="form" name="formteste" action="" method="post">
+             
+                    <input className="fild" type="text" name="nome" value="Nome" required="required"></input>
+                    <input  className="fild" type="text" name="email" value="Email" required="required"></input>
+                    <input className="fild" type="text" name="assunto" value="assunto" required="required" ></input>
+                    <textarea className="fild" name="mensagem" value="Sua mensagem"></textarea>
+                    <input className="fild" type="submit" name="enviar" value="enviar"></input>
+                    
+            </form>
+            </div>
+            </section>
+        
+        );
+    }
+}
